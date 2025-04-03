6340895a8a43f69c08e1c3d9e98fb51e786b98df
KapilMandaviya <mandaviyakapil34179@gmail.com>
2025-04-03 13:51:29 +0530

    Add project files.

---
+++ DataAccessLayer/Data/ELECTRICIADBContext.Custom.cs
@@ -0,0 +1,24 @@
+﻿
+using DataAccessLayer.DTO;
+using DataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Data
+{
+    public partial class ELECTRICIADBContext
+    {
+        public virtual DbSet<BlogDetailDTO> BlogDetailDTO { get; set; }
+        public virtual DbSet<SPCategoryList> SPCategoryList { get; set; }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BlogDetailDTO>().HasNoKey().ToView(null); // No table mapping, just for queries
+            modelBuilder.Entity<SPCategoryList>().HasNoKey().ToView(null); // No table mapping, just for queries
+        }
+    }
+}
---
+++ UtilityLayer/DTO/ProductsMaster.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UtilityLayer.DTO
+{
+
+    public partial class ProductsMaster_DTO
+    {
+        public int ProductId { get; set; }
+
+        public string? PName { get; set; }
+
+        public string? PSku { get; set; }
+
+        public string? CategoryId { get; set; }
+
+        public double? PBasePrice { get; set; }
+
+        public int? PStock { get; set; }
+
+        public string? PDescription { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+
+        public string? CreatedByUserId { get; set; }
+
+        public DateTime? UpdatedDate { get; set; }
+
+        public DateTime? DeletedDate { get; set; }
+
+        public bool? IsDelete { get; set; }
+
+        public string? ModifyAction { get; set; }
+    }
+}
